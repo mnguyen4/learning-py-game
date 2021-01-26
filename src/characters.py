794a6173b50d261ed490bdbdda377dd762060730
Minh Nguyen <minov88@gmail.com>
2021-01-26 16:33:01 -0700

    Speed increase logic and collision detection.

--- src/characters.py
+++ src/characters.py
@@ -1,16 +1,20 @@
 import pygame, sys, random
 from pygame.locals import *
 
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 600
+SPEED = 5
+
 class Enemy(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.image = pygame.image.load("resources/car2.png")
         self.surf = pygame.Surface((68, 100))
-        self.rect = self.surf.get_rect(center=(random.randint(40, 360), 0))
+        self.rect = self.surf.get_rect(center=(random.randint(40, SCREEN_WIDTH-40), 0))
     
     def move(self):
-        self.rect.move_ip(0, 10)
-        if (self.rect.bottom > 600):
+        self.rect.move_ip(0, SPEED)
+        if (self.rect.bottom > SCREEN_HEIGHT):
             self.rect.top = 0
             self.rect.center = (random.randint(30, 370), 0)
     
@@ -24,12 +28,12 @@ class Player(pygame.sprite.Sprite):
         self.surf = pygame.Surface((69, 100))
         self.rect = self.surf.get_rect(center=(150, 500))
     
-    def update(self):
+    def move(self):
         pressed_keys = pygame.key.get_pressed()
         if self.rect.left > 0:
             if pressed_keys[K_LEFT]:
                 self.rect.move_ip(-5, 0)
-        if self.rect.right < 400:
+        if self.rect.right < SCREEN_WIDTH:
             if pressed_keys[K_RIGHT]:
                 self.rect.move_ip(5, 0)
     
--- src/game-main-2.py
+++ src/game-main-2.py
@@ -1,7 +1,8 @@
 import pygame
 import sys
+import time
 from pygame.locals import *
-from characters import Enemy, Player
+from characters import *
 
 # initialization
 pygame.init()
@@ -12,22 +13,43 @@ WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
-DISPLAY_SURF = pygame.display.set_mode((400, 600))
+#SCREEN_WIDTH, SCREEN_HEIGHT = getDimension()
+DISPLAY_SURF = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 DISPLAY_SURF.fill(WHITE)
 pygame.display.set_caption("The Game")
 # draw
 p1 = Player()
 e1 = Enemy()
+# sprite group
+enemies = pygame.sprite.Group()
+enemies.add(e1)
+all_sprites = pygame.sprite.Group()
+all_sprites.add(p1)
+all_sprites.add(e1)
+# user event
+INC_SPEED = pygame.USEREVENT + 1
+pygame.time.set_timer(INC_SPEED, 1000)
 
 while True:
-    p1.update()
-    e1.move()
     DISPLAY_SURF.fill(WHITE)
-    p1.draw(DISPLAY_SURF)
-    e1.draw(DISPLAY_SURF)
-    fps.tick(60)
-    pygame.display.update()
     for event in pygame.event.get():
+        if event.type == INC_SPEED:
+            print("Speed: ", SPEED)
+            SPEED += 5
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+    # move and redraw
+    for entity in all_sprites:
+        DISPLAY_SURF.blit(entity.image, entity.rect)
+        entity.move()
+    # collision detection
+    if pygame.sprite.spritecollideany(p1, enemies):
+        DISPLAY_SURF.fill(RED)
+        pygame.display.update()
+        for entity in all_sprites:
+            entity.kill()
+        time.sleep(2)
+
+    fps.tick(60)
+    pygame.display.update()
